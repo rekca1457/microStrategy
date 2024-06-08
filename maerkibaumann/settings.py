1fad9120eb8eea9aa498ec2b6b00e9deca105526
rekca1457 <rekca@tutamail.com>
2024-06-08 23:05:59 +0300

    add new features

---
+++ maerkibaumann/settings.py
@@ -0,0 +1,13 @@
+BOT_NAME = 'maerkibaumann'
+SPIDER_MODULES = ['maerkibaumann.spiders']
+NEWSPIDER_MODULE = 'maerkibaumann.spiders'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0',
+ITEM_PIPELINES = {
+    'maerkibaumann.pipelines.DatabasePipeline': 300,
+}
+FEED_EXPORT_ENCODING = 'utf-8'
+ROBOTSTXT_OBEY = True
+
+LOG_LEVEL = 'WARNING'
+
+# LOG_LEVEL = 'DEBUG'
